ac26c238783d7f19adaf797a7c1dc2f7fb57d38a
Andrei Rotaru <andrei.rotaru@moovforge.com>
2023-01-11 17:13:13 +0200

    add simple api route for getting blog posts

---
+++ pages/api/blogs/getPosts.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import blog from "../blog.json";
+
+export default function handler(req, res) {
+  let _posts = [];
+  try {
+    const { query } = req;
+    const { page = 1, pageSize = 3, q, categoryFilter } = query;
+    const { posts: allPosts } = blog;
+    const position = (page - 1) * pageSize;
+
+    if (q) {
+    }
+
+    if (categoryFilter) {
+    }
+
+    _posts = allPosts.slice(position, position + pageSize);
+    res.status(200).json({ posts: _posts });
+  } catch (e) {}
+}
